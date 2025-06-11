38460014a00dd62658633495d703e71893aad086
KISHORE-14622 <kishoresathosh14622.com>
2025-06-11 21:27:02 +0530

    Configured Vite base path for Render deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -8,7 +8,7 @@ import GetQuoteForm from './components/GetQuoteForm';
 
 export default function App() {
   return (
-    <Router basename="/Tradingsit">
+    <BrowserRouter basename="/Tradingsit">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -16,6 +16,6 @@ export default function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/getquoteform" element={<GetQuoteForm />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
