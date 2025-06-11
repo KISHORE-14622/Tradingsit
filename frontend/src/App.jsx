91909012a7ff0becedfb31789e29ba672ba5020f
KISHORE-14622 <kishoresanthosh14622@gmail.com>
2025-06-11 17:17:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
@@ -7,14 +7,14 @@ import GetQuoteForm from './components/GetQuoteForm';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/Tradingsit">
+    <Router basename="/Tradingsit">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/getquoteform" element={<GetQuoteForm/>}/>
+        <Route path="/getquoteform" element={<GetQuoteForm />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
